b9be66ed6942eafbdb8b7d550284c3026d5124fc
Saiful Islam <saifulislam1337x@gmail.com>
2019-09-04 12:56:48 +0600

    Redirect authencated admin to dashboard function added in app/http/middleware/RedirectIfAuthenticated file and Redirect unauthenticated admin to login page functionality added in app/exception/handler.php file

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -23,4 +23,30 @@ class RedirectIfAuthenticated
 
         return $next($request);
     }
+
+
+    /**
+    * Handle an incoming request.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  \Closure  $next
+    * @param  string|null  $guard
+    * @return mixed
+    */
+    public function handle($request, Closure $next, $guard = null)
+    {
+        switch($guard){
+            case 'admin':
+                if (Auth::guard($guard)->check()) {
+                    return redirect('/admin');
+                }
+                break;
+            default:
+                if (Auth::guard($guard)->check()) {
+                    return redirect('/');
+                }
+                break;
+        }
+        return $next($request);
+    }
 }
