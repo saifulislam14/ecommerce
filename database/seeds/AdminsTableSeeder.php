8fd7a1f201de7cc6f0a3ae67dd272d141c994c21
Saiful Islam <saifulislam1337x@gmail.com>
2019-09-04 10:51:13 +0600

    admin.php,createadmintable.php,AdminTableSeeder.php file are edited...

--- database/seeds/AdminsTableSeeder.php
+++ database/seeds/AdminsTableSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Admin;
+use Faker\Factory as Faker;
 use Illuminate\Database\Seeder;
 
 class AdminsTableSeeder extends Seeder
@@ -11,6 +13,12 @@ class AdminsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Faker::create();
+ 
+        Admin::create([
+            'name'      =>  $faker->name,
+            'email'     =>  'admin@admin.com',
+            'password'  =>  bcrypt('password'),
+        ]);
     }
 }
